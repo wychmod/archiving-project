72072deab9f818f13192ae01bb127049ba1a11cd
wychmod <545480453@qq.com>
2022-10-29 17:29:20 +0800

    add: 简单工厂搭建发奖领域

---
+++ lottery-domain/src/main/java/com/wychmod/domain/award/repository/IAwardRepository.java
@@ -0,0 +1,10 @@
+package com.wychmod.domain.award.repository;
+
+/**
+ * @description: 奖品表仓储服务接口
+ * @author：wychmod
+ * @date: 2022/10/29
+ */
+public interface IAwardRepository {
+    // TODO 对分库分表中的用户中奖纪录操作
+}
--- lottery-interfaces/src/test/java/com/wychmod/test/SpringRunnerTest.java
+++ lottery-interfaces/src/test/java/com/wychmod/test/SpringRunnerTest.java
@@ -1,6 +1,13 @@
 package com.wychmod.test;
 
+import com.wychmod.Constants;
+import com.wychmod.domain.award.model.req.GoodsReq;
+import com.wychmod.domain.award.model.res.DistributionRes;
+import com.wychmod.domain.award.service.factory.DistributionGoodsFactory;
+import com.wychmod.domain.award.service.goods.IDistributionGoods;
 import com.wychmod.domain.strategy.model.req.DrawReq;
+import com.wychmod.domain.strategy.model.res.DrawResult;
+import com.wychmod.domain.strategy.model.vo.DrawAwardInfo;
 import com.wychmod.domain.strategy.service.draw.IDrawExec;
 import com.wychmod.infrastructure.dao.IActivityDao;
 import com.wychmod.infrastructure.po.Activity;
@@ -32,12 +39,40 @@ public class SpringRunnerTest {
     @Resource
     private IDrawExec drawExec;
 
+    @Resource
+    private DistributionGoodsFactory distributionGoodsFactory;
+
+
+
     @Test
     public void test_drawExec() {
         drawExec.doDrawExec(new DrawReq("小傅哥", 10001L));
         drawExec.doDrawExec(new DrawReq("小佳佳", 10001L));
         drawExec.doDrawExec(new DrawReq("小蜗牛", 10001L));
         drawExec.doDrawExec(new DrawReq("八杯水", 10001L));
+    }
+
+    @Test
+    public void test_award() {
+        // 执行抽奖
+        DrawResult drawResult = drawExec.doDrawExec(new DrawReq("小傅哥", 10001L));
+
+        // 判断抽奖结果
+        Integer drawState = drawResult.getDrawState();
+        if (Constants.DrawState.FAIL.getCode().equals(drawState)) {
+            logger.info("未中奖 DrawAwardInfo is null");
+            return;
+        }
+
+        // 封装发奖参数，orderId：2109313442431 为模拟ID，需要在用户参与领奖活动时生成
+        DrawAwardInfo drawAwardInfo = drawResult.getDrawAwardInfo();
+        GoodsReq goodsReq = new GoodsReq(drawResult.getuId(), "2109313442431", drawAwardInfo.getAwardId(), drawAwardInfo.getAwardName(), drawAwardInfo.getAwardContent());
+
+        // 根据 awardType 从抽奖工厂中获取对应的发奖服务
+        IDistributionGoods distributionGoodsService = distributionGoodsFactory.getDistributionGoodsService(drawAwardInfo.getAwardType());
+        DistributionRes distributionRes = distributionGoodsService.doDistribution(goodsReq);
+
+        logger.info("测试结果：{}", JSON.toJSONString(distributionRes));
     }
 
     @Test
